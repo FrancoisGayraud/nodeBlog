fcca754291508afc8a4d214319d8376ceb4b3942
root <root@vps643491.ovh.net>
2019-04-16 20:56:13 +0200

    edit initdb.sql

--- initdb.sql
+++ initdb.sql
@@ -1,2 +1,2 @@
-create database blog;
-create table posts (id int NOT NULL AUTO_INCREMENT primary key, date datetime, user_id int, content varchar(10000), title varchar(100));
+create table IF NOT EXISTS posts (id int NOT NULL AUTO_INCREMENT primary key, date datetime, user_id int, content varchar(10000), title varchar(100));
+create table IF NOT EXISTS user (id int NOT NULL AUTO_INCREMENT primary key, username varchar(100), password varchar(1000));
